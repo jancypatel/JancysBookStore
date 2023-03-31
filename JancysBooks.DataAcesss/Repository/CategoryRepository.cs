ff91b315df51abf97d80a098f483f917306c320a
jancypatel <113460020+jancypatel@users.noreply.github.com>
2023-03-31 13:51:12 -0400

    Update

--- JancysBooks.DataAcesss/Repository/CategoryRepository.cs
+++ JancysBooks.DataAcesss/Repository/CategoryRepository.cs
@@ -4,15 +4,22 @@ using JancysBookStore.DataAcesss.Data;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using JancysBooks.DataAccess.Repository;
 
 namespace JancysBooks.DataAcesss.Repository
 {
-    public class CategoryRepository : Repository<Category>, ICategoryRepositry
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
     {
         private readonly ApplicationDbContext _db;
         public CategoryRepository(ApplicationDbContext db) : base(db)
         {
             _db = db; 
+        }
+
+        public void Update(Category category)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- JancysBooks.DataAcesss/Repository/IRepository/ICategoryRepository.cs
+++ JancysBooks.DataAcesss/Repository/IRepository/ICategoryRepository.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using JancysBooks.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using JancysBooks.Models;
+using JancysBooks.DataAcesss.Repository.iRepository;
 
 namespace JancysBooks.DataAcesss.Repository.IRepository
 {
-    internal class ICategoryRepositry
+    public interface ICategoryRepository : IRepository<Category>
     {
+        void Update(Category category);
     }
 }
